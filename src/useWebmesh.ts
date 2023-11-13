5cd0ace01d5a537566054bf761517150cf737c4b
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-13 10:38:32 +0200

    start building out methods

--- src/useWebmesh.ts
+++ src/useWebmesh.ts
@@ -1,47 +1,113 @@
-import { useState, useEffect } from 'react';
-import { createGrpcWebTransport } from '@connectrpc/connect-web';
+import { useState, useEffect } from "react";
+import { createGrpcWebTransport } from "@connectrpc/connect-web";
 import {
-    DaemonClient,
-    Options,
-    DefaultNamespace,
-    DefaultDaemonAddress,
-} from '@webmeshproject/api/utils/daemon';
+  GetConnectionResponse,
+  PutConnectionResponse,
+  DaemonConnStatus,
+  DaemonStatus,
+  ListConnectionsResponse,
+} from "@webmeshproject/api/v1/app_pb";
+import {
+  DaemonClient,
+  Options,
+  DefaultNamespace,
+  DefaultDaemonAddress,
+} from "@webmeshproject/api/utils/daemon";
+import { Network } from "@webmeshproject/api/utils/networks";
 
 /**
  * DaemonOptions are options for connecting to a daemon process.
  */
 export class DaemonOptions extends Options {
-    constructor(opts?: Partial<DaemonOptions>) {
-        if (!opts) {
-            opts = Options.defaults();
-        }
-        opts.transport = createGrpcWebTransport({
-            baseUrl: opts.daemonAddress || DefaultDaemonAddress,
-            interceptors: [
-                Options.interceptor(opts.namespace || DefaultNamespace),
-            ],
-        });
-        super(opts);
+  constructor(opts?: Partial<DaemonOptions>) {
+    if (!opts) {
+      opts = Options.defaults();
     }
+    opts.transport = createGrpcWebTransport({
+      baseUrl: opts.daemonAddress || DefaultDaemonAddress,
+      interceptors: [Options.interceptor(opts.namespace || DefaultNamespace)],
+    });
+    super(opts);
+  }
 }
 
 /**
  * Context is the context for interacting with a webmesh daemon.
  */
 export interface Context {
-    /**
-     * client is the daemon client.
-     */
-    client: DaemonClient;
+  /**
+   * client is the daemon client.
+   */
+  client: DaemonClient;
+  /**
+   * networks is the list of networks.
+   */
+  networks: Network[];
+  /**
+   * error is an error that occurred during polling, if any.
+   */
+  error?: Error;
+  /**
+   * daemonStatus returns the current status of the daemon.
+   */
+  daemonStatus(): Promise<DaemonStatus>;
+  /**
+   * listNetworks returns the current list of networks.
+   */
+  listNetworks(): Promise<Array<Network>>;
 }
 
 /**
  * useWebmesh is a hook for interacting with a webmesh daemon.
  */
 export function useWebmesh(opts?: Partial<DaemonOptions>) {
-    const [client, setClient] = useState<DaemonClient>({} as DaemonClient);
-    useEffect(() => {
-        setClient(new DaemonOptions(opts).client());
-    }, [opts]);
-    return { client } as Context;
-};
+  const [client, setClient] = useState<DaemonClient>({} as DaemonClient);
+  const [networks, setNetworks] = useState<Network[]>([]);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  const daemonStatus = (): Promise<DaemonStatus> => {
+    return new Promise((resolve, reject) => {
+      client
+        .status({})
+        .then((resp: DaemonStatus) => {
+          resolve(resp);
+        })
+        .catch((err: Error) => {
+          reject(err);
+        });
+    });
+  };
+
+  const listNetworks = (): Promise<Array<Network>> => {
+    return new Promise((resolve, reject) => {
+      const data = new Array<Network>();
+      client
+        .listConnections({})
+        .then((resp: ListConnectionsResponse) => {
+          for (const [id, conn] of Object.entries(resp.connections)) {
+            const c = new Network(client, id, conn);
+            data.push(c);
+          }
+          setNetworks(data);
+          resolve(data);
+        })
+        .catch((err: Error) => {
+          reject(err);
+        });
+    });
+  };
+
+  let interval: NodeJS.Timeout;
+  useEffect(() => {
+    if (interval) {
+      clearInterval(interval);
+    }
+    const client = new DaemonOptions(opts).client();
+    setClient(client);
+    listNetworks().catch((err: Error) => {
+      setError(err);
+    });
+  }, [opts]);
+
+  return { client, networks, error, daemonStatus, listNetworks } as Context;
+}
