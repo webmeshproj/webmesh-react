c815f08a08eb82d97843b4977f841ad13d1c7d9d
Avi Zimmerman <avi.zimmerman@gmail.com>
2023-11-14 12:48:13 +0200

    re-export types

--- index.ts
+++ index.ts
@@ -1,3 +1,24 @@
+import {
+    Network,
+    NetworkParameters,
+    Parameters,
+    Metrics,
+} from '@webmeshproject/api/utils/networks';
+import {
+    DaemonClient,
+    Options,
+    DefaultNamespace,
+    DefaultDaemonAddress,
+} from '@webmeshproject/api/utils/daemon';
 import { DaemonOptions, Context, useWebmesh } from './src/useWebmesh';
-export type { Context };
-export { DaemonOptions, useWebmesh };
+
+export type { Parameters, Context, DaemonClient, Metrics, NetworkParameters };
+
+export {
+    DefaultNamespace,
+    DefaultDaemonAddress,
+    DaemonOptions,
+    Options,
+    Network,
+    useWebmesh,
+};
